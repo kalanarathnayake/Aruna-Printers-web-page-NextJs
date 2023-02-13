f494c616f8074570b3270aa5c646fcc72e9dee6d
kalanarathnayake <kalanagayanga8@gmail.com>
2023-02-13 19:44:26 +0530

    underdevelopment state fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -119,7 +119,7 @@ export default function Example() {
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative px-3 py-1 text-sm leading-6 text-gray-600 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Announcing our Website is Under devlopment.{' '}
-                <a href="#" className="font-semibold text-indigo-600">
+                <a href="https://media.discordapp.net/attachments/939514100361351208/1074694839213498368/under-construction-2891888__340.webp" className="font-semibold text-indigo-600">
                   <span className="absolute inset-0" aria-hidden="true" />
                   Read more <span aria-hidden="true">&rarr;</span>
                 </a>
