5ae65819b34214e769aa77eca60b01737b0a64ae
kalanarathnayake <kalanagayanga8@gmail.com>
2023-02-13 17:05:23 +0530

    updated UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -136,7 +136,7 @@ export default function Example() {
                 Email us - arunaprint@gmail.com</p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="#"
+                  href="#" 
                   className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   ___
